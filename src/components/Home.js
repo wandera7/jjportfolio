0a02ca2212b833a940e4b8c17decdc2841472741
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-08 17:46:32 +0300

    new png image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
 import React from 'react'
-import jnew from '../assets/jsnew.jpg'
+import html from '../assets/html.png'
 import {MdOutlineKeyboardArrowRight} from "react-icons/md"
 const Home = () => {
   return (
-    <div>
-        <div>
+    <div name="home" className='flex h-screen w-full bg-gradient-to-b from-black to-gray-800  '>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div>
                 <h2>
                     I'm a Full Stack Developer
                 </h2>
                 <p>
                     I have over 3 years of experience building and developing software.
-                    CUrrently, I love to work on web application using technologies like React,Tailwind,Ruby on Rails ,Node and SQL
+                    Currently, I love to work on web application using technologies like React,Tailwind,Ruby on Rails ,Node and SQL
                 </p>
+                <div>
+                    <button >
+                        Portfolio
+                        <span>
+                            <MdOutlineKeyboardArrowRight/>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div className='image'>
+                <img src={html} alt='wanderajr'/>
             </div>
         </div>
     </div>
