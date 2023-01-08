f304d37cf21da271073d32e0da6274ea0d9dbb23
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-08 14:56:17 +0300

    Changed tailwind config file

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Boss
-        </p>
-      </header>
+     <h1 className="text-">
+
+     </h1>
     </div>
   );
 }
