a920ca444b01cebd0dfc75d0f17cf1da2629ded8
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-08 17:16:56 +0300

    import arrowright icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import jnew from '../assets/jsnew.jpg'
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <div>
+            <div>
+                <h2>
+                    I'm a Full Stack Developer
+                </h2>
+                <p>
+                    I have over 3 years of experience building and developing software.
+                    CUrrently, I love to work on web application using technologies like React,Tailwind,Ruby on Rails ,Node and SQL
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
 
