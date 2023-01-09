fd70987d9db055bacba10c7e839886bd755ff9a2
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-09 14:54:48 +0300

    style about header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 function App() {
   return (
     <div>
     <NavBar/>
     <Home/>
+    <About/>
     <SocialLinks/>
     </div>
   );
