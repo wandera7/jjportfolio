f77dc59dc0d6380491a98e0a76a9fb33df6cd454
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-09 22:16:44 +0300

    data js file

---
+++ src/components/Language.js
@@ -0,0 +1,94 @@
+import bootstrap from '../assets/jnew.png'
+import cplus from '../assets/cplus.png'
+import css from '../assets/css.png'
+import django from '../assets/django.png'
+import github from '../assets/github.png'
+import html from '../assets/html.png'
+import tailwind from '../assets/tailwind.png'
+import javascript from '../assets/javascript.png'
+import node from '../assets/node.png'
+import python from '../assets/python.png'
+import reactjs from '../assets/reactjs.png'
+import ruby from '../assets/ruby.png'
+import sql from '../assets/sql.png'
+import rubyonrails from '../assets/rubyonrails.png'
+
+
+export const data =[
+    {
+        id:1,
+        source:html,
+        name:"HTML"
+    },
+    {
+        id:2,
+        source:css,
+        name:"CSS"
+    },
+    {
+        id:3,
+        source:bootstrap,
+        name:"Bootsrap"
+    },
+    {
+        id:4,
+        source:tailwind,
+        name:"Tailwind Css"
+    },
+    {
+        id:5,
+        source:javascript,
+        name:"JavaScript"
+    },
+    {
+        id:6,
+        source:reactjs,
+        name:"React"
+    },
+    {
+        id:7,
+        source:node,
+        name:"Node JS"
+    },
+    {
+        id:8,
+        source:python,
+        name:"Python"
+    },
+    {
+        id:9,
+        source:django,
+        name:"Django"
+    },
+    {
+        id:10,
+        source:ruby,
+        name:"Ruby"
+    },
+    {
+        id:11,
+        source:rubyonrails,
+        name:"Ruby on Rails"
+    },
+    {
+        id:12,
+        source:cplus,
+        name:"C++"
+    },
+    {
+        id:13,
+        source:sql,
+        name:"SQL"
+    },
+    {
+        id:13,
+        source:github,
+        name:"GitHub"
+    },
+    {
+        id:14,
+        source:html,
+        name:"HTML"
+    },
+
+]
