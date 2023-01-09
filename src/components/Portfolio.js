6dc959794cbf03c95e2cfceaf08fa7beab33a44b
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-09 20:26:21 +0300

    Skills component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,18 +2,18 @@ import React from 'react'
 import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
 const Portfolio = () => {
   return (
-    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-500 w-full text-white md:h-screen'>
-        <div className='max-w-screen-lg w-full h-full p-4 mx-auto flex flex-col justify-center'>
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p className=''>Portfolio</p>
-                <p>Check my work here</p>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+                <p className='py-6'>Check my work here</p>
             </div>
-            <div>
-                <div>
-                    <img src="" alt=''/>
-                    <div>
-                        <button></button>
-                        <button></button>
+            <div className='grid sm:grid-cols-3  lg:grid-cols-3 gap-4 px-12 sm:px-0'>
+                <div className='shadow-md shadow-gray-600 rounded-lg'>
+                    <img src={arrayDestruct} alt='im' className='rounded-md duration-200 hover:scale-105'/>
+                    <div className='flex items-center justify-center'>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105'>Demo</button>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105'>Code</button>
                     </div>
                 </div>
             </div>
