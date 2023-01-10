f8aa044c9e5dd185e99d8d3a7caf4d2f59de761d
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-10 11:15:43 +0300

    React Scroll in Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
+import { Link } from 'react-scroll';
 const NavBar = () => {
-    const links=["Home","About", "Skills" ,"Contact"];
+    const links=["Home","About", "Portfolio", "Skills" ,"Contact"];
     const [showIcon,setShowIcon] = useState(false);
   return (
     <div className='flex justify-between items-center w-full h-20 text-white px-4 bg-black fixed'>
@@ -10,7 +11,7 @@ const NavBar = () => {
         </div>
         <ul className='hidden md:flex '>
         {
-            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">{link}</li>)
+            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"><Link to={link}>{link}</Link></li>)
         }
         </ul>
         <div onClick={()=>setShowIcon(!showIcon)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
@@ -20,7 +21,7 @@ const NavBar = () => {
             showIcon && (
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
         {
-            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize py-6 text-4xl">{link}</li>)
+            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize py-6 text-4xl"><Link to={link}>{link}</Link></li>)
         }
         </ul>
             )
