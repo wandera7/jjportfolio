708deff12abd5aaacd86dd69e914da901b2652db
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-10 10:16:47 +0300

    Final Conatct component

--- src/components/Language.js
+++ src/components/Language.js
@@ -94,3 +94,11 @@ export const data =[
     },
 
 ]
+<input type="text" name="name" placeholder="Enter your name " className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
+                    <input type="text" name="email" placeholder="Enter your email " className=" my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"/>
+                    <textarea name="message" rows="10" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" placeholder="Enter your message"></textarea>
+                    <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-50 duration:300">Let's talk</button>
+
+ <form action="GET" className="flex flex-col w-full md:w-1/2">
+
+</form>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,9 +8,9 @@ const Portfolio = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
                 <p className='py-6'>Check my work here</p>
             </div>
-            <div className='grid sm:grid-cols-3  lg:grid-cols-3 gap-4 px-12 sm:px-0'>
+            <div className='grid sm:grid-cols-3  lg:grid-cols-3 gap-8 px-12 sm:px-0'>
                 <div className='shadow-md shadow-gray-600 rounded-lg'>
-                    <img src={arrayDestruct} alt='im' className='rounded-md duration-200 hover:scale-105'/>
+                    <img src={arrayDestruct} alt='im' className='rounded-md'/>
                     <div className='flex items-center justify-center'>
                         <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105'>Demo</button>
                         <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105'>Code</button>
