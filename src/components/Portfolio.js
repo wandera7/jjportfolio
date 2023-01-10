c71d4add5f75517f37179fcd1f4f65738484d192
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-10 21:04:16 +0300

    Final Project component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,21 @@
 import React from 'react'
-import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
+import moshopper from '../assets/portfolio/moshopper.png'
+import wanderabuzz from '../assets/portfolio/wanderabuzz.png'
 const Portfolio = () => {
+    const projectData =[
+        {
+            id:1,
+            source:moshopper,
+            name:"MOSHOPPER",
+            link:"https://www.canva.com/design/DAFQO64fpHk/UPSJOpmXnrFvSH6X0oez_g/view?utm_content=DAFQO64fpHk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+        },
+        {
+            id:2,
+            source:wanderabuzz,
+            name:"WANDERA BUZZ",
+            link:"https://docs.google.com/presentation/d/19fLLQ-e3ttuRXWRn6tZrMA5EU-yEa7hALHXpR_u0Yic/edit?usp=sharing"
+        }
+    ]
   return (
     <div name="Projects" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
@@ -8,14 +23,17 @@ const Portfolio = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Projects</p>
                 <p className='py-6'>Check my work here</p>
             </div>
-            <div className='grid sm:grid-cols-3  lg:grid-cols-3 gap-8 px-12 sm:px-0'>
-                <div className='shadow-md shadow-gray-600 rounded-lg'>
-                    <img src={arrayDestruct} alt='im' className='rounded-md'/>
-                    <div className='flex items-center justify-center'>
-                        <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105'>Demo</button>
-                        <button className='w-1/2 px-6 py-3 m-4 duration-300 hover:scale-105'>Code</button>
-                    </div>
+            <div className='grid sm:grid-cols-3  lg:grid-cols-3 gap-8 px-12 sm:px-0 text-center'>
+            {
+                projectData.map(({id,source,name,link})=>{
+                    return(
+                <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                    <img src={source} alt={name} className='rounded-md h-25 mx-auto'/>
+                    <a href={link} className='m-4'>{name}</a>
                 </div>
+                    )
+                })
+            }
             </div>
         </div>
     </div>
