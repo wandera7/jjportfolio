03aff3ab56604bacc52d5dde98cb21ba09e82ecb
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-10 11:46:14 +0300

    React scroll in Home component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
         </div>
         <ul className='hidden md:flex '>
         {
-            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"><Link to={link}>{link}</Link></li>)
+            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"><Link to={link} smooth duration={500}>{link}</Link></li>)
         }
         </ul>
         <div onClick={()=>setShowIcon(!showIcon)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
@@ -21,7 +21,7 @@ const NavBar = () => {
             showIcon && (
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
         {
-            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize py-6 text-4xl"><Link to={link}>{link}</Link></li>)
+            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize py-6 text-4xl"><Link onClick={()=>setShowIcon(!showIcon)} smooth duration={500} to={link}>{link}</Link></li>)
         }
         </ul>
             )
