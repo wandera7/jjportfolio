7086ceb1b8944f497655b37fff417a5e4540cc2f
wandera7 <julius.wandera@student.moringaschool.com>
2023-01-10 21:29:05 +0300

    Navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,9 +9,9 @@ const NavBar = () => {
         <div>
             <h1 className='text-5xl font-signature ml-2'>Wanderajr</h1>
         </div>
-        <ul className='hidden md:flex '>
+        <ul className='hidden lg:flex '>
         {
-            links.map((link,index)=><li key={index} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"><Link to={link} smooth duration={500}>{link}</Link></li>)
+            links.map((link,index)=><li key={index} className="px-6 text-xl cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"><Link to={link} smooth duration={500}>{link}</Link></li>)
         }
         </ul>
         <div onClick={()=>setShowIcon(!showIcon)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
